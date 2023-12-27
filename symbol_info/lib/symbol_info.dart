8a26ee32e996efc0f48c70f75af59c79886bf50c
vinod-fyers <100824281+vinod-fyers@users.noreply.github.com>
2023-12-28 00:09:09 +0530

    prototyping on base class

--- symbol_info/lib/symbol_info.dart
+++ symbol_info/lib/symbol_info.dart
@@ -6,8 +6,16 @@ import 'package:symbol_master/models/symbol_data_model.dart';
 
 /// A Calculator.
 class SymbolInfo {
-  static Widget displaySymbolInfoBanner(
-      SymbolDataModel symbol, String comingFrom) {
-    return SymbolInfoBanner(comingFrom: "", symbol: symbol);
+  static final SymbolInfo _symbolInfo = SymbolInfo._internal();
+  SymbolInfo._internal();
+  factory SymbolInfo() {
+    return _symbolInfo;
   }
+
+  static SymbolInfoBanner symbolInfoBanner(
+          {required SymbolDataModel? symbol, required String comingFrom}) =>
+      SymbolInfoBanner(
+        comingFrom: comingFrom,
+        symbol: symbol,
+      );
 }
