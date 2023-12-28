b499e4a5c89f9bc7bc0268146f11d93bc67d7749
vinod-fyers <100824281+vinod-fyers@users.noreply.github.com>
2023-12-28 14:24:39 +0530

    removed create alert folders

--- create_alert/example/create_alert_sample/android/app/src/main/kotlin/com/example/create_alert_sample/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.create_alert_sample
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- create_alert/lib/constants/utils/sso.dart
+++
@@ -1,9 +0,0 @@
-class SSOBloc {
-  static final SSOBloc instance = SSOBloc._internal();
-  factory SSOBloc() => instance;
-  SSOBloc._internal() : super();
-
-  String accessToken = "";
-
-  String get token => "";
-}
